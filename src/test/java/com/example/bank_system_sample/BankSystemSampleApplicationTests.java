486e5493e879931d94cc05dcaf871e1f42b9b90d
IzanAlvaroForte <yullarjhonmora@gmail.com>
2026-09-17 09:24:27 +0800

    initial commit

---
+++ src/test/java/com/example/bank_system_sample/BankSystemSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_system_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
